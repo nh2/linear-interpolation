8e0b31f61dcba865ed9086918c52dc8a51054692
Niklas Hambüchen <mail@nh2.me>
2013-02-16 17:07:21 +0000

    Cleanup + a gnuplot

--- LinearInterpolation.hs
+++ LinearInterpolation.hs
@@ -1,4 +1,4 @@
-import Debug.Trace
+import Graphics.Gnuplot.Simple
 
 type Coord1 = Double
 type Coord2 = (Double, Double)
@@ -16,30 +16,13 @@ interpolate1 (l, lv) (r, rv) x = lv + steep * (x - l)
     steep = (rv - lv) / (r - l)
 
 
--- interpolate2 :: Point2 -> Point2 -> Point2 -> Point2 -> Coord2 -> Double
--- interpolate2 ((a1, b1), v1) ((a2, b2), v2) ((a3, b3), v3) ((a4, b4), v4) (ax, bx)
-
 interpolate2 :: Bound -> Bound -> (V, V, V, V) -> Coord2 -> V
 interpolate2 (a0, a1) (b0, b1) (a0b0, a0b1, a1b0, a1b1) (ax, bx)
   | not (a0 < a1)              = error "a bounds invalid"
   | not (b0 < b1)              = error "b bounds invalid"
-  | not (a0 <= ax && ax <= a1) = error "ax not in bounds"
-  | not (b0 <= bx && bx <= b1) = error "bx not in bounds"
-  | otherwise                  = traceShow ("dista", dista) $
-                                 traceShow ("distb", distb) $
-                                 traceShow ("distax", distax) $
-                                 traceShow ("distbx", distbx) $
-                                 traceShow ("fraca", fraca) $
-                                 traceShow ("fracb", fracb) $
-                                 traceShow ("steepOna0", steepOna0) $
-                                 traceShow ("steepOna1", steepOna1) $
-                                 traceShow ("steepOnb0", steepOnb0) $
-                                 traceShow ("steepOnb1", steepOnb1) $
-                                 traceShow ("starta0", starta0) $
-                                 traceShow ("startb0", startb0) $
-                                 traceShow ("enda1", enda1) $
-                                 traceShow ("endb1", endb1) $
-                                 f
+  | not (a0 <= ax && ax <= a1) = error $ "ax not in bounds " ++ show ax
+  | not (b0 <= bx && bx <= b1) = error $ "bx not in bounds " ++ show bx
+  | otherwise                  = f
   where
     dista = a1 - a0
     distb = b1 - b0
@@ -83,4 +66,12 @@ test = and
   , interpolate2test1 (0.5, 0  ) == 1.5
   , interpolate2test1 (0.5, 1  ) == 3.5
   , interpolate2test1 (1  , 0.5) == 2.5
+
+  -- Middle
+  , interpolate2test1 (0.5, 0.5) == 2.5
   ]
+
+
+main = let xs = [0,0.1..0.9::Double] ++ [1.0]
+        -- in plotFunc3d [] [] xs xs (\x y -> exp(-(x*x+y*y)))
+        in plotFunc3d [] [] xs xs (\x y -> interpolate2test1 (x, y))
